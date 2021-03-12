7d8e36015477245e89d363d10cea59e9ad11acec
Matt Newman <matthewnewman0806@gmail.com>
2021-03-12 00:29:08 +0000

    Commit

---
+++ src/ChatScreen.js
@@ -0,0 +1,70 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState } from "react";
+import "./ChatScreen.css";
+
+function ChatScreen() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      name: "test",
+      image: "...",
+      message: "Yoooo",
+    },
+    {
+      name: "test",
+      image: "...",
+      message: "sike",
+    },
+
+    {
+      message: "Howdy",
+    },
+  ]);
+
+  const handleSend = (e) => {
+    e.preventDefault();
+    setMessages([...messages, { message: input }]);
+    setInput("");
+  };
+
+  return (
+    <div className="chatScreen">
+      <p className="chatScreen__timestamp">YOU LIKED SMALLFOOT</p>
+      {messages.map((message) =>
+        message.name ? (
+          <div className="chatScreen__message">
+            <Avatar
+              className="chatScreen__image"
+              alt={message.name}
+              src={message.image}
+            />
+            <p className="chatScreen__text">{message.message}</p>
+          </div>
+        ) : (
+          <div className="chatScreen__message">
+            <p className="chatScreen__textUser">{message.message}</p>
+          </div>
+        )
+      )}
+
+      <form className="chatScreen__input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="chatScreen__inputField"
+          placeholder="Type a message..."
+          type="text"
+        />
+        <button
+          onClick={handleSend}
+          type="submit"
+          className="chatScreen__inputButton"
+        >
+          SEND
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ChatScreen;
